76889ff8f3d9d3473bfd95a1d807ebba3a77648e
Quang Nghĩa <89003923+quangnghia1110@users.noreply.github.com>
2025-01-08 12:53:24 +0700

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://wolf_db_jy7c_user:bQZGgft7izXwvfqWT3KXPe5LsIrgwDZF@dpg-ctv13456147c739qk3n0-a:5432/wolf_db_jy7c'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://wolf_db_jy7c_user:bQZGgft7izXwvfqWT3KXPe5LsIrgwDZF@dpg-ctv13456l47c739qk3n0-a.oregon-postgres.render.com/wolf_db_jy7c'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'your-secret-key-here'
 
