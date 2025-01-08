3c24aa24798b5fd249c03f262d950df86f0f7723
Quang Nghĩa <89003923+quangnghia1110@users.noreply.github.com>
2025-01-08 13:21:34 +0700

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://wolf_db_jy7c_user:bQZGgft7izXwvfqWT3KXPe5LsIrgwDZF@dpg-ctv13456l47c739qk3n0-a.oregon-postgres.render.com/wolf_db_jy7c'
@@ -7,5 +8,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'your-secret-key-here'
 
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 from app import routes, models 
